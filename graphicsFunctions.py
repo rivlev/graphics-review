8a254b8449d222db4f3d05e2bf620f34986d48f0
rivlev <proflevitan@gmail.com>
2017-03-13 23:47:26 -0400

    fix bug

--- graphicsFunctions.py
+++ graphicsFunctions.py
@@ -8,7 +8,7 @@ def normalize(v):
 	return v / magnitude(v)
 
 def getNormal(vertices):
-	normalize(numpy.cross(vertices[1]-vertices[0], vertices[2]-vertices[0]))
+	return normalize(numpy.cross(vertices[1]-vertices[0], vertices[2]-vertices[0]))
 
 def reflect(v, normal):
 	return 2 * normal * (v.dot(normal)) - v
